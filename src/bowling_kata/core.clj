32348ca6b7c742df6ecd7a5c578b3a79f18ed78e
Gwenael Ropert <gwena.code@gmail.com>
2018-04-12 17:43:29 +0200

    Feature: Add bowling game score functions

---
+++ src/bowling_kata/core.clj
@@ -0,0 +1,53 @@
+(ns bowling-kata.core
+  (:require [clojure.string :as str]))
+
+(def re-standard-frame #"\| (\d \d|\d /|X) ")
+(def re-last-frame #"\| (\d \d|\d / (\d|X)|X (\d \d|\d /|X (\d|X))) \|")
+(def re-9-frames (re-pattern (str "^((" re-standard-frame "){9})")))
+(def re-all-frames (re-pattern (str re-9-frames re-last-frame \$)))
+
+(defn ->int [digit]
+  (Integer/parseInt digit))
+
+(defn spare->down-pins [first-throw]
+  (str first-throw " " (- 10 (->int first-throw))))
+
+(defn frame->throws [frame]
+  (-> frame
+      (str/replace #"(\d) \/" #(spare->down-pins (second %)))
+      (str/replace #"X" "10")
+      (str/split #" ")
+      (->> (map ->int))))
+
+(defn score-frame [down-pins frames]
+  (if (= 10 down-pins)
+    (apply + (take 3 (flatten frames)))
+    down-pins))
+
+(defn score-frames [all-frames]
+  (loop [frames all-frames, acc 0]
+    (let [down-pins (apply + (first frames))]
+      (if (= 1 (count frames))
+        (+ acc down-pins)
+        (recur (rest frames)
+               (+ acc (score-frame down-pins frames)))))))
+
+(defn score-game [game]
+  (let [matches (re-matches re-all-frames game)]
+    (assert (some? matches) "Bowling game information format not respected!")
+    (let [standard-frames (->> (second matches)
+                               (re-seq re-standard-frame)
+                               (map second))
+          last-frame (nth matches 4)
+          all-frames (concat standard-frames [last-frame])
+          all-throws (map frame->throws all-frames)]
+      (score-frames all-throws))))
+
+(defn score-game-file [filename]
+  (-> (slurp filename)
+      (str/trim-newline)
+      score-game))
+
+(defn -main [& args]
+  (assert (not= 1 (count args) "Take only one argument: filename with line to score"))
+  (println (score-game-file (first args))))
